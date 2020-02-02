734097e2113a8e30323156d7e57092e6abfee338
Vladislav <ivanovvvmain@yandex.ru>
2020-02-02 13:38:54 +0700

    new build

---
+++ precache-manifest.a00f5580705ba0f915d3d1808b213786.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b70a931654ac462c32cc975b1a60fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95032fdc7caad727c883",
+    "url": "/static/css/main.60530cb2.chunk.css"
+  },
+  {
+    "revision": "13bc1c268f9e10b02edf",
+    "url": "/static/js/2.b5220e4a.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/static/js/2.b5220e4a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "95032fdc7caad727c883",
+    "url": "/static/js/main.be88ff8d.chunk.js"
+  },
+  {
+    "revision": "070db8a3c8ae3e511328",
+    "url": "/static/js/runtime-main.2546e0bf.js"
+  }
+]);
